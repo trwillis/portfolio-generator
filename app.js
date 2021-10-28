a1c06643431e0a8747f64f288cd71fa8320e2e98
Tracee <signuntothee@gmail.com>
2021-10-27 22:56:25 -0400

    module 916

---
+++ app.js
@@ -0,0 +1,18 @@
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Tracee',
+  age: 46
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
+  printProfileData(profileDataArr);
+
+  //
